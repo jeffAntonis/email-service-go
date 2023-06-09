9940ccc262c4c2cc3a155ab1f5b218738e162999
jeffAntonisCamino <jefferson.antonis@caminoeducation.com>
2023-06-09 11:01:58 -0300

    refactor: add xid to NewCampaign

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -1,6 +1,10 @@
 package campaign
 
-import "time"
+import (
+	"time"
+
+	"github.com/rs/xid"
+)
 
 type Contact struct {
 	Email string
@@ -21,7 +25,7 @@ func NewCampaign(name string, content string, emails []string) *Campaign {
 	}
 
 	return &Campaign{
-		Id:        "1",
+		Id:        xid.New().String(),
 		Name:      name,
 		Content:   content,
 		CreatedOn: time.Now(),
