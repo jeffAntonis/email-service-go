ceec2573ea0654fe125985f6ae7628437409ccc2
jeffAntonisCamino <jefferson.antonis@caminoeducation.com>
2023-06-09 11:02:29 -0300

    test: improve campaign_test

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -2,20 +2,40 @@ package campaign
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewCampaign(t *testing.T) {
+var (
+	name     = "Campaign X"
+	content  = "Body"
+	contacts = []string{"email1@e.com", "email2@e.com"}
+)
+
+func Test_NewCampaign_CreateCampaign(t *testing.T) {
 	assert := assert.New(t)
-	name := "Campaign X"
-	content := "Body"
-	contacts := []string{"email1@e.com", "email2@e.com"}
 
 	campaign := NewCampaign(name, content, contacts)
 
-	assert.Equal(campaign.Id, "1")
 	assert.Equal(campaign.Name, name)
 	assert.Equal(campaign.Content, content)
 	assert.Equal(len(campaign.Contacts), len(contacts))
+}
+
+func Test_NewCampaign_IdIsNotNill(t *testing.T) {
+	assert := assert.New(t)
+
+	campaign := NewCampaign(name, content, contacts)
+
+	assert.NotNil(campaign.Id)
+}
+
+func Test_NewCampaign_CreatedOnMustBeNow(t *testing.T) {
+	assert := assert.New(t)
+	now := time.Now().Add(-time.Minute)
+
+	campaign := NewCampaign(name, content, contacts)
+
+	assert.Greater(campaign.CreatedOn, now)
 }
