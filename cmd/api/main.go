cae2c0860c7b921fd1b1e11634d2bfa4b302aa12
jeffAntonisCamino <jefferson.antonis@caminoeducation.com>
2023-06-13 14:29:00 -0300

    chore: add air

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,18 +1,48 @@
 package main
 
 import (
+	"email-service-go/internal/contract"
+	"email-service-go/internal/domain/campaign"
+	"email-service-go/internal/infrastructure/database"
+	internalerros "email-service-go/internal/internal-erros"
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
 )
 
 func main() {
 	fmt.Println("Iniciando ...")
 	r := chi.NewRouter()
-	r.Get("/{productName}", func(w http.ResponseWriter, r *http.Request) {
-		param := chi.URLParam(r, "productName")
-		w.Write([]byte(param))
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	service := campaign.Service{
+		Repository: &database.CampaignRepository{},
+	}
+	r.Post("/campaigns", func(w http.ResponseWriter, r *http.Request) {
+		var request contract.NewCampaign
+		render.DecodeJSON(r.Body, &request)
+		id, err := service.Create(request)
+
+		if err != nil {
+
+			if errors.Is(err, internalerros.ErrInternal) {
+				render.Status(r, 500)
+			} else {
+				render.Status(r, 400)
+			}
+			render.JSON(w, r, map[string]string{"error": err.Error()})
+			return
+		}
+		render.Status(r, 201)
+		render.JSON(w, r, map[string]string{"id": id})
 	})
 
 	http.ListenAndServe(":3000", r)
